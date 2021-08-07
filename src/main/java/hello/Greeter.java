d75ade1b07e45b3698406761fd7d7149a3c5420e
mvnaresh <38608007+mvnaresh@users.noreply.github.com>
2021-08-07 09:11:05 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is M Venkata Naresh your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+    return "Hello world- This is Venkata Naresh Mandhala your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
